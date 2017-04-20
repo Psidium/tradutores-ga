4936507585cbb153807cfcbb12b4a3c4e5dc0e10
Gabriel Nunes <gabriel.nunes@sap.com>
2017-04-19 22:24:41 -0300

    Implements floats, operations, comments, and fixes overall code

--- tradutor_lexico.py
+++ tradutor_lexico.py
@@ -3,62 +3,95 @@
 import sys
 import re
 
+
 class AttributionExpression:
-    def __init__(self, matcher):
-        self.type = matcher[1];
-        self.name = matcher[2];
-        self.value = matcher[3];
+    def __init__(self, tokens):
+        self.type = tokens.group(1)
+        self.name = tokens.group(2)
+        self.right_side = tokens.group(3)
 
     def get_tokens(self):
-        return [
-            "[reserved_word, " + self.type + "]",
-            "[id, " + self.name + "]",
-            "[Equal_Op,=]",
-            "[value, " + self.value + "]",
-        ]
+        return ['[reserved_word, ' + self.type + ']', '[id, ' + self.name + ']',
+                '[equal_op, =]'] + self.compute_right_side()
+
+    def compute_right_side(self):
+        expression = re.compile('(?:([-+*/])\s*((?:[-+])?\d+(?:\.\d+)?)|((?:[-+])?\d+(?:\.\d+)?))')
+        matches = expression.findall(self.right_side)
+        tokens = []
+
+        for (operator, number, numberOnly) in matches:
+            if numberOnly:
+                tokens.append('[num, ' + numberOnly + ']')
+            else:
+                tokens.append('[arith_op, ' + operator + ']')
+                tokens.append('[num, ' + number + ']')
+
+        return tokens
+
 
 class DefinitionExpression:
-    def __init__(self, matcher):
-        self.type = matcher[1];
-        self.name = matcher[2];
+    def __init__(self, tokens):
+        self.type = tokens.group(1)
+        self.name = tokens.group(2)
 
     def get_tokens(self):
-        return [
-            "[reserved_word, " + self.type + "]",
-            "[id, " + self.name + "]"
-        ]
+        return ['[reserved_word, ' + self.type + ']', '[id, ' + self.name + ']']
 
-reserved_words = [ 'printf', 'int' ];
-operators = {
-    '=' : 'Equal_Operator'
-}
 
-attribution = re.compile('(int)\s(\w+)[\s|=]+(\d);')
-definition = re.compile('(int)\s(\w+);')
+# Script
+expressions = [{
+    'regex': re.compile('(int|float)\s+(\w(?:(?:\w|\d)+)?)\s*=\s*((?:[-+])?\d+(?:\.\d+)?\s*(?:(?:[-+*/]\s*(?:(?:[-+])?\d+(?:\.\d+)?))+)?)\s*;'),
+    'tokenizer': AttributionExpression
+}, {
+    'regex': re.compile('(int|float)\s+(\w(?:(\w|\d)+)?)\s*;'),
+    'tokenizer': DefinitionExpression
+}]
+
+
+def normalize_spaces(source):
+    return re.sub(r'(?!\n)\s+', ' ', source)  # We don't need to consider strings
+
+
+def remove_comments(source):
+    return re.sub(r'(//.+?$|/\*(.|\s)+?\*/)', '', source)
+
+
+def remove_empty_lines(split_source):
+    return [l for l in split_source if l]
+
+
+def clean_source(source):
+    return normalize_spaces(remove_comments(source))
+
 
 def read_program_from_file(path):
     f = open(path)
-    return f.read().split('\n')
+    source = f.read()
+    return remove_empty_lines(clean_source(source).split('\n'))
 
-def get_splitter(expression):
-    result = attribution.match(expression)
-    if (result != None):
-        return AttributionExpression(result)
-    result = definition.match(expression)
-    if (result != None):
-        return DefinitionExpression(expression)
+
+def get_tokenizer(program_line):
+    for expression in expressions:
+        tokens = expression['regex'].match(program_line)
+        if tokens is not None:
+            return expression['tokenizer'](tokens)
+
     return None
 
-if (len(sys.argv) < 2):
-    print("Fuck you")
+
+def print_tokens(tokens):
+    print(' '.join(tokens))
+
+
+if len(sys.argv) < 2:
+    print('Wrong number of arguments passed.')
     sys.exit()
 
-tokens = []
 program_lines = read_program_from_file(sys.argv[1])
-for line in program_lines:
-    splitter = get_splitter(line)
-    if splitter != None:
-        print(splitter.get_tokens())
-    else:
-        print("Failed to compute expression: \"" + line + "\"")
 
+for line in program_lines:
+    tokenizer = get_tokenizer(line)
+    if tokenizer is not None:
+        print_tokens(tokenizer.get_tokens())
+    else:
+        print('Failed to compute expression: "' + line + '"')
